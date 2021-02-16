02704c31c5e1fd9ebc349ee9acbf3e1b3c0e9d26
BrunoPer3s <brunoperes7@hotmail.com>
2021-02-16 20:30:50 -0300

    Challenge Finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -54,12 +54,26 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', {
+      id,
+    });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const response = await api.get('/foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
+
+      setFoods(
+        response.data.map((food: Food) => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        })),
+      );
     }
 
     loadFoods();
@@ -67,14 +81,20 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get('/categories');
+      setCategories(response.data);
     }
 
     loadCategories();
   }, []);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    if(selectedCategory === id) {
+      setSelectedCategory(undefined);
+    }
+      else {
+        setSelectedCategory(id)
+      }
   }
 
   return (
@@ -83,6 +103,7 @@ const Dashboard: React.FC = () => {
         <Image source={Logo} />
         <Icon
           name="log-out"
+          style={{transform: [{rotateY: '180deg'}]}}
           size={24}
           color="#FFB84D"
           onPress={() => navigation.navigate('Home')}
